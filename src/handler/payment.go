df47ca7f9758584db57735be871b405bca2fcc95
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-12-14 18:22:00 +0100

    feat(payment-entity): add db_url and use gorm to connect to the db

--- src/handler/payment.go
+++ src/handler/payment.go
@@ -3,6 +3,9 @@ package handler
 // import local package payment in go/src/payment
 import (
 	"go/src/payment"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Response struct {
@@ -20,3 +23,9 @@ func NewPaymentHandler(paymentService payment.Service) *paymentHandler {
 }
 
 //TODO: Implement the handler methods here
+
+func (ps *paymentHandler) Test(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "test",
+	})
+}
