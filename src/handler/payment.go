ae629f097a99b7e76c1a177136aead1bc966bbb1
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-12-14 22:20:07 +0100

    feat(payment-entity): start handler WIP

--- src/handler/payment.go
+++ src/handler/payment.go
@@ -1,6 +1,5 @@
 package handler
 
-// import local package payment in go/src/payment
 import (
 	"go/src/payment"
 	"net/http"
@@ -8,7 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Response struct {
+type PaymentResponse struct {
 	Success bool
 	Message string
 	Data    interface{}
@@ -24,8 +23,55 @@ func NewPaymentHandler(paymentService payment.Service) *paymentHandler {
 
 //TODO: Implement the handler methods here
 
-func (ps *paymentHandler) Test(c *gin.Context) {
+func (ph *paymentHandler) TestPayment(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "test",
+	})
+}
+
+// func Create payment in bdd using PaymentResponse
+func (ph *paymentHandler) Create(c *gin.Context) {
+	var input payment.InputPayment
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, PaymentResponse{
+			Success: false,
+			Message: "Cannot extract JSON body",
+			Data:    err.Error(),
+		})
+		return
+	}
+
+	// doesnt work because of the foreign key
+	payment, err := ph.paymentService.Create(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, PaymentResponse{
+			Success: false,
+			Message: "Something went wrong",
+			Data:    err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, PaymentResponse{
+		Success: true,
+		Message: "Payment created",
+		Data:    payment,
+	})
+}
+
+func (ph *paymentHandler) GetAll(c *gin.Context) {
+	payments, err := ph.paymentService.GetAll()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, PaymentResponse{
+			Success: false,
+			Message: "Something went wrong",
+			Data:    err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, PaymentResponse{
+		Success: true,
+		Message: "All payments",
+		Data:    payments,
 	})
 }
