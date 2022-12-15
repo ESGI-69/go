3df7cd71bc939a68ed7793e024d095a9e93e56cf
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-12-15 19:51:46 +0100

    feat(product): add unique key on name

--- src/product/entity.go
+++ src/product/entity.go
@@ -4,7 +4,7 @@ import "time"
 
 type Product struct {
 	ID        int       `gorm:"primaryKey"`
-	Name      string    `gorm:"type:varchar(255)"`
+	Name      string    `gorm:"uniqueIndex;type:varchar(255)"`
 	Price     float64   `gorm:"type:float"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
