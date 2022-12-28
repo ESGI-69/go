6b73a5f30b4b54648391842b8d1e75646faa5e39
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-12-28 09:48:46 +0100

    feat(5-add-new-endpoint): creation payment form done

--- src/js/payment.js
+++ src/js/payment.js
@@ -1,27 +1,43 @@
-const createPayment = async (event) => {
+// const createPayment = async (event) => {
+//   event.preventDefault();
+//   const paymentData = new FormData(document.getElementById('payment-form'))
+//   try {
+//     const result = await fetch('http://localhost:3000/api/payments', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         PricePaid: parseFloat(paymentData.get('PricePaid')),
+//         ProductId: parseInt(paymentData.get('ProductId')),
+//       }),
+//     });
+//     await result.json();
+//     window.location.href = '/';
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+function createPayment(event) {
   event.preventDefault();
   const paymentData = new FormData(document.getElementById('payment-form'))
-  console.log(paymentData)
-  console.log('PricePaid', (paymentData.get('PricePaid')))
-  console.log('ProductId', (paymentData.get('ProductId')))
-  try {
-    const result = await fetch('http://localhost:3000/api/payments', {
-      method: 'POST',
-      // headers: {
-      //   'Content-Type': 'application/json',
-      // },
-      body: paymentData,
-    });
-    console.log(result)
-    console.log(result.json)
-    console.log(result.body)
-    await result.json();
-    // redirect to /
-    // window.location.href = '/';
-  } catch (error) {
-    console.error(error)
-  }
-
+  fetch('http://localhost:3000/api/payments/', {
+    method: 'POST',
+    body: JSON.stringify({
+      PricePaid: parseFloat(paymentData.get('PricePaid')),
+      ProductId: parseInt(paymentData.get('ProductId')),
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      window.location.href = '/';
+    }
+    )
+    .catch((error) => {
+      console.error('Error:', error);
+    }
+    );
 }
 
 const deletePayment = async (paymentId) => {
